71da30bec3ebe324b8fd8fa45f3699fbcc074d1b
CARLA GEOVANA MOLINA BRUNETTO <carla.geovana23@gmail.com>
2023-10-22 21:24:37 -0300

    refatoração

--- js/script.js
+++ js/script.js
@@ -16,3 +16,17 @@ function mudouTamanho(){
        
     }
 }
+
+$('.carousel-item', '.carousel-multiple').each(function(){
+    var next = $(this).next();
+    if (! next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+}).each(function(){
+    var prev = $(this).prev();
+    if (! prev.length) {
+        prev = $(this).siblings(':last');
+    }
+    prev.children(':nth-last-child(2)').clone().prependTo($(this));
+});
